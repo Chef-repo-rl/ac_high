50639067523ba9385a093ed7fc259a4bc6d9c7eb
ashishraj91 <ashishraj65@gmail.com>
2019-11-15 12:39:06 -0800

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,8 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+include_recipe 'ac_high::ac_10_2'
+include_recipe 'ac_high::ac_2_2'
+include_recipe 'ac_high::ac_17_2'
+include_recipe 'ac_high::ac_2_5'
