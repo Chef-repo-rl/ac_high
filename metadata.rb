dec978bb3f384e6d69b4996fb43032e28c951f0c
ashishraj91 <ashishraj65@gmail.com>
2019-11-18 11:24:00 -0800

    changed metadata

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,19 @@
 name 'ac_high'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
+maintainer 'Relevance Lab'
+maintainer_email 'ashish.raj@relevancelab.com'
+license 'Apache-2.0'
 description 'Installs/Configures ac_high'
 long_description 'Installs/Configures ac_high'
 version '0.1.3'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/ac_high/issues'
+issues_url 'https://github.com/relevancelab/ac_high/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/ac_high'
+source_url 'https://github.com/relevancelab/ac_high'
+supports 'redhat'
